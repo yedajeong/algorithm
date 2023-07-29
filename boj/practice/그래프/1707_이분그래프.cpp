c7bbf5f05379c9bd98267e0d53a93fa8f843bfed
yedajeong <49023751+yedajeong@users.noreply.github.com>
2023-07-29 22:44:19 +0900

    Create 1707_이분그래프.cpp

---
+++ boj/practice/그래프/1707_이분그래프.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int k, v, e;
+
+bool BFS(int v, vector<vector<int>> (&A), vector<int> (&visited)){
+    queue<int> myQue;
+    myQue.push(v);
+    int color = 1;
+    visited[v] = color;
+
+    while(!myQue.empty()){
+        int now = myQue.front();
+        myQue.pop();
+        color = visited[now] * -1;
+        cout << "현재 노드 / 현재 색 / 다음 색: " << now << visited[now] << color << endl;
+
+        for (int next : A[now]){
+            if (visited[next] == 0){
+                myQue.push(next);
+                visited[next] = color;
+            }
+            else if (visited[next] == visited[now]){
+                cout << next << " " << now << endl;
+                cout << visited[next] << endl;
+                return false;
+            }
+                
+        }
+    }
+
+    return true;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a, b;
+
+    cin >> k;
+
+    for (int test_case = 0; test_case < k; ++test_case){
+        cin >> v >> e;
+
+        vector<vector<int>> A(v+1);
+        vector<int> visited(v+1);
+
+        // 그래프 구성
+        for (int i=0; i<e; ++i){
+            cin >> a >> b;
+            A[a].push_back(b);
+            A[b].push_back(a);
+        }
+
+        bool result;
+        for (int i=1; i<=v; ++i){
+            if (!visited[i])
+                result = BFS(i, A, visited);
+
+            if (!result){
+                cout << "NO\n";
+                break;
+            }
+        }
+
+        if (result)
+            cout << "YES\n";
+    }
+    
+    return 0;
+}
